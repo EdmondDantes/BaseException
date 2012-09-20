cccde12735a383fb5c04339158df50a7f2885c90
Edmond <if@ifcastle.com>
2012-09-20 10:07:51 +0300

    + Callable exception for function is_callable
    Use it exception for cases when expression should be called but did not
    can.

---
+++ source/CallableException.php
@@ -0,0 +1,29 @@
+<?PHP
+namespace Exceptions;
+
+/**
+ * Rais if the expression is not callable.
+ * (Usually when using function is_callable)
+ */
+class CallableException extends LoggableException
+{
+    /**
+     * Expression is not callable!
+     *
+     * @param       mixed        $expression     Expression
+     */
+    public function __construct($expression)
+    {
+        if(is_object($expression) || is_resource($expression))
+        {
+            $expression = self::truncate($expression);
+        }
+
+        parent::__construct
+        ([
+            'message'       => 'Expression is not callable',
+            'expression'    => $expression
+        ]);
+    }
+}
+?>
