a79b38bae9245c098f150da42c0135d0c15b38c9
Edmond <if@ifcastle.com>
2012-09-24 20:23:27 +0300

    % Added param "reason" for SerializeException

--- source/SerializeException.php
+++ source/SerializeException.php
@@ -9,21 +9,23 @@ class SerializeException  extends LoggableException
     /**
      * Object can't be serialized!
      *
-     * @param       object|array        $object         The object which must have been serialized
+     * @param       string|array        $reason         Reason of error
+     * @param       object              $object         The object which must have been serialized
      * @param       string              $type           Type of serialize
      * @param       object              $src_object     The object where started the process
      */
-    public function __construct($object = null, $type = 'phpserialize', $src_object = null)
+    public function __construct($reason, $object = null, $type = 'phpserialize', $src_object = null)
     {
-        if(is_array($object))
+        if(!is_string($reason))
         {
-            parent::__construct($object);
+            parent::__construct($reason);
             return;
         }
 
         parent::__construct
         ([
             'message'       => 'Serialize Failed',
+            'reason'        => $reason,
             'type'          => $type,
             'object'        => $this->get_value_type($object),
             'src_object'    => $this->get_value_type($src_object)
