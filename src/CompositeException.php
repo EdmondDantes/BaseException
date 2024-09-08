e5146a868a0df34b0f8befaf6cfa88f196d58434
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-09-08 11:11:08 +0300

    + CompositeException

---
+++ src/CompositeException.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace IfCastle\Exceptions;
+
+class CompositeException            extends LoggableException
+{
+    public function __construct(string $message = 'Multiple exceptions occurred', \Throwable ...$exceptions)
+    {
+        $previous                   = null;
+        
+        if(!empty($exceptions[0])) {
+            $previous               = $exceptions[0];
+        }
+        
+        parent::__construct([
+            'template'              => 'Multiple exceptions occurred {context}',
+            'context'               => $message,
+            'exceptions'            => $exceptions,
+            'previous'              => $previous
+        ]);
+    }
+}
